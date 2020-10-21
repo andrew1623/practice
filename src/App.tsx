1330b320e23727bea0c5893d9b6afd8640539328
A. Thompson <Andrew.Thompson3@my.unt.edu>
2020-10-21 16:45:26 -0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+        <ul>
+          <li>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              React Docs
+            </a>
+          </li>
+          <li>
+            <a
+              className="App-link"
+              href="https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/class_components"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              React-TS Cheatsheet
+            </a>
+          </li>
+        </ul>
+      </body>
     </div>
   );
 }
